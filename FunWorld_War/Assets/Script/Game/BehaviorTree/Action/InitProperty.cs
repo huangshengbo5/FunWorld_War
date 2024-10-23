a39b4b4ce23ead12d26ee74cb16e3851f2ae38c3
SK-20220725NFXU\huangshengbo <m15137737050@163.com>
2024-10-23 14:17:16 +0800

    补充基础AI行为树

--- FunWorld_War/Assets/Script/Game/BehaviorTree/Action/InitProperty.cs
+++ FunWorld_War/Assets/Script/Game/BehaviorTree/Action/InitProperty.cs
@@ -9,13 +9,7 @@ namespace BehaviorDesigner.Runtime.Tasks
 
         public override void OnBehaviorRestart()
         {
-            //TestBranch
             base.OnBehaviorRestart();
-<<<<<<< HEAD
-            
-=======
-            //this.Owner. selfTrans.SetValue(this.GameObject.transform);
->>>>>>> 7398cbc59f316b1f066607f9c571d0c750119c0f
         }
 
         public override void OnFixedUpdate()
